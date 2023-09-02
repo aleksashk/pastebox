3d0966dbf859e6419d57bda7dd23009a37eb332e
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-02 21:07:12 +0300

    add PasteBoxController @GetMapping("/")

--- src/main/java/dev/aleksandrphilimonov/pastebox/controller/PasteBoxController.java
+++ src/main/java/dev/aleksandrphilimonov/pastebox/controller/PasteBoxController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
+import java.util.Collections;
+
 @RestController
 public class PasteBoxController {
 
@@ -11,4 +14,11 @@ public class PasteBoxController {
     public String getByHash(@PathVariable String hash) {
         return hash;
     }
+
+    @GetMapping("/")
+    public Collection<String> getPublicPasteList() {
+        return Collections.emptyList();
+    }
+
+
 }
